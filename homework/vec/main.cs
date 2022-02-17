729d27e9395cfc52c93938fce0b846b3aeb4ff79
martalj <96marta@gmail.com>
2022-02-17 16:04:32 +0100

    homework vec, B

--- homework/vec/main.cs
+++ homework/vec/main.cs
@@ -1,6 +1,18 @@
+using static System.Console;
+
 public class main{
 	static void Main(){
-		vec u = new vec(100,200,300);
-		u.print("u=");
+		vec u= new vec(100,200,300);
+		u.print("u = ");
+		vec v= new vec(1,2,3);
+		v.print("v = ");
+		(u+v).print("u+v = ");
+		(u-v).print("u-v = ");
+		(4*v).print("4*v = ");
+		var w = v*4;
+		w.print("v*4 = ");
+		(vec.prod(u,v)).print("u cross prod v = ");
+		WriteLine($"u dot c = {vec.dot(u,v)}");
+		WriteLine($"norm(u) = {vec.norm(u)}");
 	}
 }
--- homework/vec/vec.cs
+++ homework/vec/vec.cs
@@ -19,4 +19,7 @@ public class vec{
 	//methods:
 	public void print(string s){Write(s);WriteLine($"{x} {y} {z}");}
 	public void print(){this.print("");}
+	public static double dot(vec u, vec v){return (u.x*v.x+u.y*v.y+u.z*v.z);}
+	public static vec prod(vec u, vec v){return new vec(u.y*v.z-u.z*v.y,u.z*v.x-u.x*v.z,u.x*v.y-u.y*v.x);}
+	public static double norm(vec u){return (Abs(u.x)+Abs(u.y)+Abs(u.z));}
 }
