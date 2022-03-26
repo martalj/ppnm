fed598c13dfcd362118b6d9ad4d59e8d5cfcbbf1
martalj <96marta@gmail.com>
2022-03-26 16:58:33 +0100

    homework linear equations, A1

--- homework/13-lineq/A/gs.cs
+++ homework/13-lineq/A/gs.cs
@@ -1,10 +1,19 @@
 using static System.Console;
 using static System.Math;
 
-class gs{
-	public static void QRGSdecomp(matrix A, matrix R){
-		int m = A.size2; 
+public class gs{
+	public static matrix QRGSdecomp(matrix A, matrix R){
+		int m = A.size2;
+		matrix Q = new matrix(A.size1,m);
 		for(int i=0;i<m;i++){
-			
+			R[i,i]=A[i].norm();
+			Q[i] = A[i]/R[i,i];
+			for(int j=i+1;j<m;j++){
+				R[i,j] = Q[i].dot(A[j]);
+				A[j] -= R[i,j]*Q[i];
+				}
+		}	
+		A.print("A"); R.print("R"); Q.print("Q");
+	return Q;
 	}
 }
--- homework/13-lineq/A/main.cs
+++ homework/13-lineq/A/main.cs
@@ -4,6 +4,22 @@ using static System.Math;
 
 class main{
 	static void Main(){
+		var rand = new System.Random();
+		int n = (int) 10;
+		int m = (int) 5;
+		var A = new matrix(n,m);
+		for (int i=0; i<n; i++){
+			for (int j=0; j<m; j++){
+				A[i, j] = rand.NextDouble();
+			}
+		}
+	matrix R = new matrix(m,m);
+	matrix Q = gs.QRGSdecomp(A,R);
+	matrix QTQ = Q.transpose()*Q;
+	matrix QR = Q*R;
+	
+	QTQ.print("Q^T*Q");
+	QR.print("Q*R");
 	}
 
 }
