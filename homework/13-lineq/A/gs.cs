ee3d6dbabd3aad9b33bbb86e29912b3b3d7bb089
martalj <96marta@gmail.com>
2022-03-31 16:29:54 +0200

    homework lineqA part 1 done - problem in part 2

--- homework/13-lineq/A/gs.cs
+++ homework/13-lineq/A/gs.cs
@@ -5,12 +5,13 @@ public class gs{
 	public static matrix QRGSdecomp(matrix A, matrix R){
 		int m = A.size2;
 		matrix Q = new matrix(A.size1,m);
+		matrix v = A.copy();
 		for(int i=0;i<m;i++){
-			R[i,i]=A[i].norm();
-			Q[i] = A[i]/R[i,i];
+			R[i,i]=v[i].norm();
+			Q[i] = v[i]/R[i,i];
 			for(int j=i+1;j<m;j++){
 				R[i,j] = Q[i].dot(A[j]);
-				A[j] -= R[i,j]*Q[i];
+				v[j] -= R[i,j]*Q[i];
 				}
 		}	
 		A.print("A"); R.print("R"); Q.print("Q");
@@ -19,7 +20,7 @@ public class gs{
 
 	public static vector QRGSsolve(matrix Q, matrix R, vector b){
 		matrix QT = Q.transpose();
-		vector x = QT*b; 
+		vector x = QT*b;
 		for(int n=1;n<x.size;n++){
 			x[n-1] = (x[n-1] - R[n-1,n])/R[n-1,1];
 		}
