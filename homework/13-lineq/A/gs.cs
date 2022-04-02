f3478898fcbcf7bdb94ac4ebafa56f44b16d0b6a
martalj <96marta@gmail.com>
2022-04-02 12:59:03 +0200

    homework 13-lineq A done

--- homework/13-lineq/A/gs.cs
+++ homework/13-lineq/A/gs.cs
@@ -11,19 +11,17 @@ public class gs{
 				R[i,j] = Q[i].dot(Q[j]);
 				Q[j] -= R[i,j]*Q[i];
 				}
-		}	
-		//A.print("A"); R.print("R"); Q.print("Q");
+			}	
 	}
 
 	public static vector QRGSsolve(matrix Q, matrix R, vector b){
-		matrix QT = Q.transpose();
-		vector x = QT*b;
-		for(int i=x.size-1;i>=0;i--){
-			double sum = 0;
-			for(int n=i+1;n<x.size;n++){
+		vector x = Q.T*b;
+		for(int i=x.size-1; i>=0; i--){
+			double sum=0;
+			for(int n=i+1; n<x.size; n++){
 				sum += R[i,n]*x[n];
 				}
-			x[i] = (x[i] - sum)/R[i,i];
+			x[i] = (x[i]-sum)/R[i,i];
 			}
 	return x;
 	}
--- homework/13-lineq/A/main.cs
+++ homework/13-lineq/A/main.cs
@@ -10,32 +10,45 @@ class main{
 		var A = new matrix(n,m);
 		for (int i=0; i<n; i++){
 			for (int j=0; j<m; j++){
-				A[i, j] = rand.NextDouble();
+				A[i, j] = rand.Next(0,10);
+				}
 			}
+		
+		matrix A_sqr = new matrix(n,n);
+		for (int i=0; i<n; i++){
+			for (int j=0; j<n; j++){
+				A_sqr[i, j] = rand.Next(0,10);
+				}
+			}
+		
+		
+		A.print("A");
+		matrix R = new matrix(m,m);
+		matrix Q = A.copy();
+		gs.QRGSdecomp(A,R,Q);
+		matrix QT = Q.transpose();
+		matrix QTQ = Q.transpose()*Q;
+		matrix QR = Q*R;
+				
+		QTQ.print("Q^T*Q");
+		QR.print("Q*R");
+		R.print("R");
+		Q.print("Q");
+
+		//vector b = new vector(1,3,5,4);
+		vector b = new vector(n);
+		for(int i=0;i<n;i++){
+			b[i] = rand.Next(0,10);
+			}
+		A_sqr.print("A_sqr");
+		b.print("b");
+		matrix R_sqr = new matrix(n,n);
+		matrix Q_sqr = A_sqr.copy();
+		gs.QRGSdecomp(A_sqr,R_sqr,Q_sqr);
+		vector x = gs.QRGSsolve(Q_sqr,R_sqr,b);
+		x.print("x");
+		vector c = A_sqr*x;
+		c.print("A_sqr*x");
 		}
-
-	matrix R = new matrix(m,m);
-	matrix Q = A.copy();
-	gs.QRGSdecomp(A,R,Q);
-	matrix QT = Q.transpose();
-	matrix QTQ = Q.transpose()*Q;
-	matrix QR = Q*R;
-			
-	QT.print("Q^T");
-	QTQ.print("Q^T*Q");
-	QR.print("Q*R");
-	R.print("R");
-	Q.print("Q");
-
-	vector b = new vector(1,3,5,4);
-	//for(int i=0;i<n;i++){
-	//	b[i] = rand.NextDouble();
-	//	}
-	b.print("b");
-	vector x = gs.QRGSsolve(Q,R,b);
-	x.print("x");
-	vector c = A*x;
-	c.print("A*x");
-	}
 
 }
