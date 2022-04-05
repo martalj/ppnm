335dbc50069fc1ce77b9f02bc49eacfdd598008f
martalj <96marta@gmail.com>
2022-04-05 15:09:47 +0200

    homework 14A least squares basically done - halflife time not calculated

--- homework/14-lsqr/A/leastsq.cs
+++ homework/14-lsqr/A/leastsq.cs
@@ -1,19 +1,16 @@
-'using static System.Math;
+using static System.Math;
 using static System.Console;
+using System;
 
 public class leastsquares{
-	double f(int i, double z){
-		if(i==0) return 1;
-		if(i==1) return z;
-		else return WriteLine("Not a number");
-		}
-
-	var fs = new Func<double,double>[]{
-		z=>1.0, z=>z, z=>Log(z);
-		}
+//	double f(int i, double z){
+//		if(i==0) return 1;
+//		if(i==1) return z;
+//		else return Write("Not a number");
+//		}
 	
-	public static vector lsfit(Func<double, double> fs, double[] x, double[] y, double[] dy){
-		int n = x.size, m = fs.Length;
+	public static vector lsfit(Func<double, double>[] fs, double[] x, double[] y, double[] dy){
+		int n = x.Length, m = 3;
 		var A = new matrix(n,m);
 		var b = new vector(n);
 		for(int i=0;i<n;i++){
@@ -25,7 +22,8 @@ public class leastsquares{
 		matrix Q = A.copy();
 		matrix R = new matrix(m,m);
 		gs.QRGSdecomp(A,R,Q);
-		var c = gs.QRGS(Q,R,b);
-		}	
-	return c;
+		var c = gs.QRGSsolve(Q,R,b);
+		
+		return c;
+		}
 }
--- homework/14-lsqr/A/main.cs
+++ homework/14-lsqr/A/main.cs
@@ -1,16 +1,38 @@
 using System;
 using static System.Console;
+using static System.Math;
+
 public class main{
-public static void Main(){
-        var list = new genlist<double[]>();
-        char[] delimiters = {' ','\t',','};
-        var options = StringSplitOptions.RemoveEmptyEntries;
-	var xs = ReadLine();
-	var ys = ReadLine();
-	var dy = ReadLine();
-	xs.Split(delimiters,options);
-	ys.Split(delimiters,options);
-	dy.Split(delimiters,options);
-	WriteLine($"{xs[1]}");
+	public static void Main(){
+	        var list = new genlist<double[]>();
+	        char[] delimiters = {' ','\t',','};
+	        var options = StringSplitOptions.RemoveEmptyEntries;
+		for(string line = ReadLine();line != null; line = ReadLine()){
+			var data = line.Split(delimiters, options);
+			int n = data.Length;
+			var numbers = new double[n];
+			for(int i=0;i<n;i++){
+				numbers[i]=double.Parse(data[i]);
+				}
+			list.push(numbers);
+			}
+		var xs = list.data[0];
+		var ys = list.data[1];
+		var dy = list.data[2];
+		for(int i=0;i<xs.Length;i++){
+			WriteLine($"{xs[i]} {ys[i]} {dy[i]}");
+			}		
+
+		var fs = new Func<double,double>[] {z=>1.0, z=>z, z=>z*z};
+
+		vector c = leastsquares.lsfit(fs,xs,ys,dy);
+		
+		WriteLine();		
+		WriteLine();
+
+		for(double i = 0;i<15;i+=0.2){
+			WriteLine($"{i} {c[0]+i*c[1]+i*i*c[2]}");			
+			}
+		
 	}//Main function
 }//class
