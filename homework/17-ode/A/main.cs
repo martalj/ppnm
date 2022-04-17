88dc6e06e0623d18add2df71864153e9154def06
martalj <96marta@gmail.com>
2022-04-17 19:57:37 +0200

    ode

--- homework/17-ode/A/main.cs
+++ homework/17-ode/A/main.cs
@@ -8,16 +8,18 @@ class main{
 
 	//double b=0.25, c=5;
 
-	Func<double,vector,vector> F = delegate(double t, vector u){
-		double v = u[1];  
-	return new vector(-u);
+	Func<double,vector,vector> F = delegate(double t, vector u){  
+	return new vector(u[1],-u[0]);
 	};
 
-	vector y0 = new vector(1,1);
-	double start = 0, stop = 10;
-	vector res = rk.driver(F,start,y0,stop);
-
-	res.print("res = ");
+	var xs = new genlist<double>();
+	var ys = new genlist<vector>();
+	vector y0 = new vector(0,1);
+	double start = 0, stop = 2*PI;
+	rk.driver(F,start,y0,stop,xs,ys);
+	for(double i=0; i<=xlist; i++){
+		WriteLine($"{xs[i]} {ys[i][0]} {ys[i][1]}");
+	}//for
 
 	}//Main
 }//Class
