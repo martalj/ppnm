2901edefba31940f6eea7f69178cafa5f11f46be
martalj <96marta@gmail.com>
2022-04-17 21:02:55 +0200

    homework 17-ODE A done

--- homework/17-ode/A/ode.cs
+++ homework/17-ode/A/ode.cs
@@ -12,23 +12,17 @@ public class rk{
 	}//rkstep12
 
 	public static vector driver(
-			Func<double,vector,vector> F, double a, vector ya, double b, genlist<double> xlist = null, genlist<vector> ylist = null, 
+			Func<double,vector,vector> F, double a, vector ya, double b, 
 			double h=0.01, double acc = 0.01, double eps=0.01){
 		if(a>b) throw new Exception("driver: a>b");
 		double x=a; vector y=ya;
-		if(xlist!=null){xlist.clear();xlist.push(x);}
-		if(ylist!=null){ylist.clear();ylist.push(y);}
 		do{
 			if(x>=b) return y;
 			if(x+h>b) h=b-x;
 			var (yh,erv) = rkstep12(F,x,y,h);
 			double tol = Max(acc,yh.norm()*eps)*Sqrt(h/(b-a));
 			double err = erv.norm();
-			if(err<=tol){
-				x+=h;y=yh;
-				if(xlist!=null)xlist.push(x);
-				if(ylist!=null)ylist.push(y);
-			}//if
+			if(err<=tol){x+=h;y=yh;}
 			h*= Min(Pow(tol/err,0.25)*0.95,2);
 		}while(true);
 	}//driver
