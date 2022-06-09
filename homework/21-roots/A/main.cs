84270b3a4b8d52a61ffcab7a82b7b59663601223
martalj <96marta@gmail.com>
2022-06-09 14:26:58 +0200

    21-roots A started - gives NaN values?

--- homework/21-roots/A/main.cs
+++ homework/21-roots/A/main.cs
@@ -1,14 +1,14 @@
-using System;
 using static System.Console;
 using static System.Math;
+using System;
 
 class main{
 	public static void Main(){
 	        Func<vector,vector> f=delegate(vector x){
-	            vector sol=new vector(x[0]*x[0]*x[1], 5*x[0] + Cos(x[1]));
+	            vector sol=new vector(x[0]*x[0],0);
 	            return sol;};
 
-		vector x0 = new vector(1,1);
+		vector x0 = new vector(1,0);
 		vector x1 = roots.newton(f,x0);
 		WriteLine($"Roots: {x1[0]} {x1[1]}. Evaluating roots {f(x1)[0]} {f(x1)[1]}");
 	} //Main
--- homework/21-roots/A/rootfinding.cs
+++ homework/21-roots/A/rootfinding.cs
@@ -3,32 +3,44 @@ using static System.Console;
 using static System.Math;
 
 public class roots{
-	public static vector newton(Func<vector,vector> f, vector x0, double eps=1e-2){
+	public static matrix jacobian(Func<vector,vector> f, vector x0){
 		int n=f(x0).size;
 		int m=x0.size;
 		matrix J = new matrix(n,m);
 		vector deltax = new vector(x0.size);
-		for(int i=0;i<deltax.size;i++){
-			deltax[i] = Abs(x0[i])+Pow(2,-26);
+		for(int i=0; i<deltax.size; i++){
+			deltax[i] = x0[i]+Pow(2,-26);
 		} //for
 
-		for(int j=0;j<n;j++){
+		for(int j=0; j<n; j++){
 			for(int k=0; k<m; k++){
-				vector df = f(x0+deltax)-f(x0);
+				vector xshift = new vector(deltax.size);
+				xshift[k] = deltax[k];
+				vector xprime = x0+xshift;
+				vector df = f(xprime)-f(x0);
 				J[j,k] = df[j]/deltax[k];
 			} //for k
 		} //for j
-		while(f(x0).norm()>eps){
+
+		return J;
+	} //jacobian
+
+	public static vector newton(Func<vector,vector> f, vector x0, double eps=1e-2){
+		do{
+			matrix J = jacobian(f,x0);		
+			int m=x0.size;
 			matrix Q = J.copy();
 			matrix R = new matrix(m,m);
 			gs.QRGSdecomp(J,R,Q);
-			vector Deltax = gs.QRGSsolve(Q,R,f(x0));
+			vector Deltax = gs.QRGSsolve(Q,R,-f(x0));
 			double lambda = 1;
-			while(f(x0+lambda*Deltax).norm()>(1-lambda/2)*f(x0).norm() && lambda<1.0/32){
+			do{
 				lambda/=2;
-			} //while
+			}while(f(x0+lambda*Deltax).norm()>(1-lambda/2)*f(x0).norm() && lambda>1.0/32);
 			x0+=lambda*Deltax;
-		} //while
+		}while(f(x0).norm()>eps);
+
 		return x0;
+
 	} //newton
 } //class
