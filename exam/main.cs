c2e73af6e674d42da1dc93e20cc36e778016885a
martalj <96marta@gmail.com>
2022-06-15 15:53:53 +0200

    exam part 1+2 (estimating derivatives) done

--- exam/main.cs
+++ exam/main.cs
@@ -4,6 +4,22 @@ using static System.Math;
 
 class main{
 	static void Main(){
-	}
+		var x = new double[10];
+		var y = new double[10];
+		for(int i=0;i<x.Length;i++){
+			x[i] = i;
+			y[i] = i*i;
+			WriteLine($"{x[i]} {y[i]}");
+		}
+		
+		WriteLine();
+		WriteLine();
 
-}
+		double[] p;
+		p = subspline.inner(x,y);
+		for(int j=0;j<p.Length;j++){
+			WriteLine($"{j} {p[j]}");
+		}
+
+	}//Main
+}//class
--- exam/spline.cs
+++ exam/spline.cs
@@ -3,8 +3,37 @@ using static System.Console;
 using static System.Math;
 
 public class subspline{
-	public subspline(double[] x, double[] y){
-		
+	public static double[] inner(double[] x, double[] y){
+		int n = x.Length;
+		double[] p = new double[n];
 
-	}//subspline
+		for(int i=1;i<n-1;i++){
+			double[] x_inner = new double[3];
+			double[] y_inner = new double[3];
+
+			x_inner[0] = x[i-1];
+			x_inner[1] = x[i];
+			x_inner[2] = x[i+1];
+			y_inner[0] = y[i-1];
+			y_inner[1] = y[i];
+			y_inner[2] = y[i+1];
+			
+			
+			qspline spline_inner = new qspline(x_inner,y_inner);
+
+			if(i == 1){
+				p[i-1] = spline_inner.derivative(i-1);
+			}
+
+			p[i] = spline_inner.derivative(i);
+
+			if(i == n-2){
+				p[i+1] = spline_inner.derivative(i+1);
+			}
+
+		} //for i
+
+	return p;
+	} //inner
+
 }//class
