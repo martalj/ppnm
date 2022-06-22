9e5c9d1ab1a5b4e503d3bb89fdf854c526611c8b
martalj <96marta@gmail.com>
2022-06-22 21:06:21 +0200

    homework 21-roots A done

--- homework/21-roots/A/main.cs
+++ homework/21-roots/A/main.cs
@@ -10,6 +10,20 @@ class main{
 
 		var x0 = new vector(-1.1);
 		vector x1 = roots.newton(f,x0);
-		WriteLine($"The root is at {x1[0]}. The function in this point yields {f(x1)[0]} ");
+		WriteLine("Searching for roots for the function 2x+3:");
+		WriteLine($"The root is at {x1[0]}. The function in this point yields {f(x1)[0]}");
+
+		WriteLine();
+		WriteLine();
+
+		Func<vector,vector> R=delegate(vector x){
+		return new vector(-2*(1-x[0])-400*(x[1]-x[0]*x[0]), 200*(x[1]-x[0]*x[0]));
+		};
+
+		var xR0 = new vector(2,8);
+		var xR = roots.newton(R,xR0);
+		WriteLine("Searching for extremums of Rosenbrock's valley function by searching for the roots of its gradient:");
+		WriteLine($"The root is at {xR[0]},{xR[1]}. The function in this point yields {R(xR)[0]},{R(xR)[1]}");
+
 	} //Main
 } //class
