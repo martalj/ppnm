a61be4c9663849440a09ff2a57db6e4003c77061
martalj <96marta@gmail.com>
2022-06-25 12:54:08 +0200

    homework 22-minimization A done

--- homework/22-minimization/A/main.cs
+++ homework/22-minimization/A/main.cs
@@ -4,7 +4,30 @@ using static System.Math;
 
 class main{
 	static void Main(){
+		Func<vector,double> r = delegate(vector args){
+			return (1-args[0])*(1-args[0]) +100*(args[1]-args[0]*args[0])*(args[1]-args[0]*args[0]);
+		};	
 
+		vector x0 = new vector(2);
+		x0[0] = 1;
+		x0[1] = 1;
+		vector x1;
+		int s;
+		(x1,s) = minimize.qnewton(r,x0);
+		WriteLine($"Rosenbrock minimum is at {x1[0]},{x1[1]}, found in {s} steps");
+
+		Func<vector,double> h = delegate(vector args){
+			return (args[0]*args[0]+args[1]-11)*(args[0]*args[0]+args[1]-11) + (args[0]+args[1]*args[1]-7)*(args[0]+args[1]*args[1]-7);
+		};
+		
+		vector x2 = new vector(2);
+		x2[0] = 1;
+		x2[1] = 1;
+		vector x3;
+		int t;
+		(x3,t) = minimize.qnewton(h,x0);
+		WriteLine($"Himmelblau minimum is at {x3[0]},{x3[1]}, found in {t} steps");
+		
 	}
 
 }
--- homework/22-minimization/A/minimize.cs
+++ homework/22-minimization/A/minimize.cs
@@ -3,48 +3,59 @@ using static System.Console;
 using static System.Math;
 
 public class minimize{
-	public static vector gradient(Func<vector,vector> f, vector x){
+	public static vector gradient(Func<vector,double> f, vector x){
 		vector gx  = new vector(x.size);
 		vector x0 = x.copy();
 		for(int i=0; i<x.size; i++){
 			double dx = Abs(x[i])*Pow(2,-26);
-			x[i] += dx;
-			gx[i] = (f(x)-f(x0))/dx;
-			x[i] -= dx;
+			if(Abs(x[i])<Sqrt(Pow(2,-26))){
+				dx = Pow(2,-26);
+			}
+			
+			x0[i] += dx;
+			gx[i] = (f(x0)-f(x))/dx;
 		} //for
 	return gx;
 	} //gradient
 
-	public static vector qnewton(Func<vector,double> f, vector start, double acc = 1e-3){
-		matrix B = matrix.id(start.size);
-		vector gx = gradient(f,start);
-		vector fx = f(x);
-		while(gx.norm() > acc){
+	public static (vector,int) qnewton(Func<vector,double> f, vector x, double acc = 1e-3){
+		matrix B = matrix.id(x.size);
+		vector gx = gradient(f,x);
+		double fx = f(x);
+		int steps = 100000;
+		int s = 0;
+		vector z;
+		double fz;
+		while(s < steps){
+			s++;
 			vector dx = -B*gx;
 			double lambda = 1;
 			while(true){
+				s++;
 				z = x+lambda*dx;
 				fz = f(z);
 				if(fz < fx){
 					break;
 				}
-				if(lambda<Pow(2,-26);
-					B = matrix.id(start.size);
+				if(lambda<Pow(2,-26)){
+					B.set_identity();
 					break;
 				}
 				lambda/=2;
 			} //while true
-			vector s = z-x;
 			vector gz = gradient(f,z);
 			vector y = gz-gx;
-			vector u =s-B*y;
+			vector u =lambda*dx-B*y;
 			double uTy = u%y;
-			B.update(u,u,u/uTy);
+			if(Abs(uTy)>1e-6){
+				B.update(u,u,1/uTy);
+			}
 			x=z;
 			gx=gz;
 			fx=fz;
-		} // while > acc
-	return x;
+			if(Abs(gx.norm())<acc){break;}
+		} // while steps
+	return (x,s);
 	} //qnewton
 
 } //class
